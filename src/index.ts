d4b12c9430687e6e6cb72b82de77bc9fcb7c88a4
Chinedu Eze <ochuzor@gmail.com>
2020-02-15 12:46:05 +0200

    add basic interfaces for start

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export function add(a: number, b: number) {
-    return a + b;
+type IdType = number | string;
+
+interface TodoDoc {
+    id: IdType;
+    description: string;
+    isCompleted: string;
+    priority: string;
+    dateOfCreation: string;
+    dateOfCompletion: string;
+    projects: string;
+    contexts: string;
+    tags: string;
+}
+
+interface ITodoIndexer {
+	addDoc(doc: TodoDoc): void;
+	getDoc(id: IdType): TodoDoc;
+	deleteDoc(id: IdType): void;
+    getAll(): TodoDoc[];
+    search(query: object): TodoDoc[];
 }
