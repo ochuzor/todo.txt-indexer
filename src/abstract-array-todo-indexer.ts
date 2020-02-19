7b38c897d0b9b82157f8c5f153a187f41e4a562e
Chinedu Eze <ochuzor@gmail.com>
2020-02-19 09:48:37 +0200

    added abstract array todo indexer

---
+++ src/abstract-array-todo-indexer.ts
@@ -0,0 +1,45 @@
+import { IdType, ITodoDoc as ITodo , ITodoIndexer} from './todo-doc-types';
+
+export abstract class AbstractArrayTodoIndexer implements ITodoIndexer {
+    constructor(protected _data: ITodo[] = []) { }
+
+    NextId(): IdType {
+        return this._data.length;
+    }
+
+    addDoc(doc: ITodo): void {
+        const index = this._data.findIndex(d => d.id === doc.id);
+        const cp = Object.assign({}, doc);
+        if (index !== -1) {
+            this._data.splice(index, 1, cp);
+        }
+        else {
+            this._data.push(Object.assign({}, cp));
+        }
+    }
+
+    getDoc(id: IdType): ITodo {
+        const doc = this._data.find(doc => doc.id === id);
+        if (!doc)
+            return { id, text: '' };
+        return Object.assign({}, doc);
+    }
+
+    deleteDoc(id: IdType): void {
+        const index = this._data.findIndex(d => d.id === id);
+        if (index !== -1) {
+            this._data.splice(index, 1);
+        }
+    }
+
+    getAll(): ITodo[] {
+        return this._data.map(doc => {
+            return {
+                id: doc.id,
+                text: doc.text,
+            };
+        });
+    }
+    
+    abstract search(query: string): ITodo[];
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { IdType, ITodoDoc as ITodo , ITodoIndexer} from './todo-doc-types';
+export {AbstractArrayTodoIndexer} from './abstract-array-todo-indexer'
