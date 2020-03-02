6961b473f8afa45b3d3ad33b4a6ed78e864bc920
Chinedu Eze <ochuzor@gmail.com>
2020-03-02 21:56:06 +0200

    removed unnecessary dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { IdType, ITodoDoc, ITodoIndexer } from './todo-doc-types';
-export { AbstractArrayTodoIndexer } from './abstract-array-todo-indexer';
-export { FuseJsTodoIndexer } from './fuse-js-todo.indexer';
+export * from './todo-doc-types';
+export * from './abstract-array-todo-indexer';
+export * from './fuse-js-todo.indexer';
